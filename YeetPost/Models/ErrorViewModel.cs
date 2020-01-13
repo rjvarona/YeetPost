a7ecfe9676b2416e995b7a6ca2788f34bdd2f00b
Rudson Varona <rjvarona16@gmail.com>
2020-01-13 17:09:35 -0500

    Add project files.

---
+++ YeetPost/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace YeetPost.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
