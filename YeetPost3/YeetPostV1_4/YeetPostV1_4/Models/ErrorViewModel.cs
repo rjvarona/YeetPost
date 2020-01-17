7b80b63864a76d6fcd3bbf67ceee91f51d6edf8a
Rudson Varona <rjvarona16@gmail.com>
2020-01-16 22:12:02 -0500

    Added Last New Project is All Good with the world

---
+++ YeetPost3/YeetPostV1_4/YeetPostV1_4/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace YeetPostV1_4.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ YeetPost3/YeetPostV1_4/YeetPostV1_4/Program.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace YeetPostV1_4
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+            var x = 3;
+        }
+
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
